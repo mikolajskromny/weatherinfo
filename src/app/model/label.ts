b87838051f7c529e9b3c82f6ef6f172bb04f077d
Mikolaj <skromnymikolaj@gmail.com>
2020-06-16 10:42:41 +0200

    Delete all cities close weather and forecast Added current location city to local storage Added enum for button labels

---
+++ src/app/model/label.ts
@@ -0,0 +1,8 @@
+export enum WeatherButtonLabel {
+  showActualWeather = 'Show actual weather',
+  showActualForecast = 'Show actual forecast',
+  updateWeather = 'Update weather',
+  updateForecast = 'Update forecast',
+  closeWeather = 'Close weather',
+  closeForecast = 'Close forecast',
+}
